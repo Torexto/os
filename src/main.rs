0023441b6d5dca07cfab889630cf737fd970cd7b
torexto <tomasztrela8@gmail.com>
2026-09-20 16:02:53 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,15 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use bootloader::entry_point;
+use os::OS;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
+    OS::run();
 }
